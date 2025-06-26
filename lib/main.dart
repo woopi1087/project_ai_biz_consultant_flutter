2334499d653d95ff95c9bcbe26f02c5516fcd230
woopi1030 <lozenkrantz@gmail.com>
2025-06-26 21:49:18 +0900

    [ADD] env 추가

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,17 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(); // 환경변수 로드
+  
+  await dotenv.load(); // ✅ 환경변수 로드
+
+  final clientId = dotenv.env['NAVER_CLIENT_ID'];
+  if (clientId == null) {
+    throw Exception('NAVER_CLIENT_ID가 .env에 없음');
+  }
 
   await FlutterNaverMap().init(
-    clientId: dotenv.env['NAVER_CLIENT_ID'],  // 예: 31j0mmqupg
-    onAuthFailed: (ex) {
-      print('네이버 지도 인증 실패: $ex');
-    },
+    clientId: clientId,
+    onAuthFailed: (ex) => print('네이버 지도 인증 실패: $ex'),
   );
 
   runApp(const MyApp());
